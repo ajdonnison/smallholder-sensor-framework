904df68812379822313512352221276078a3538a
Adam Donnison <ajdonnison@gmail.com>
2014-02-26 18:14:16 +1100

    Added debug mode to water heater
    To bench test without connecting up the temperature sensors
    this debug code adds the ability to manually trigger the
    relay.

--- arduino/sketches/WaterHeater/WaterHeater.ino
+++ arduino/sketches/WaterHeater/WaterHeater.ino
@@ -30,43 +30,84 @@ float maxTemp = 95.0;
 float tank, panel;
 int light = -1;
 int lowLight = 500;
+boolean debug = true;
 
 // SakiManager manager("HW", 3, 1, true);
 // SoftwareSerial ser(SERIAL_RX, SERIAL_TX);
 OneWire oneWire(ONE_WIRE_IF);
 DallasTemperature sensors(&oneWire);
 
-DeviceAddress tankThermometer = { 0x28, 0xbe, 0xb2, 0x97, 0x04, 0x0, 0x0, 0x3f };
-DeviceAddress panelThermometer = { 0x28, 0xc7, 0xff, 0x97, 0x04, 0x0, 0x0, 0x34 };
+DeviceAddress tankThermometer = { 
+  0x28, 0xbe, 0xb2, 0x97, 0x04, 0x0, 0x0, 0x3f };
+DeviceAddress panelThermometer = { 
+  0x28, 0xc7, 0xff, 0x97, 0x04, 0x0, 0x0, 0x34 };
 
 void checkInputs() {
+  boolean manual_override = false;
+
   sensors.requestTemperatures();
   tank = sensors.getTempC(tankThermometer);
   panel = sensors.getTempC(panelThermometer);
-//  light = analogRead(LIGHT_SENSOR);
-  if (/*light > lowLight && */ panel > minTemp && (panel - tank) > minDiff) {
-    digitalWrite(RELAY, LOW);
-  } else {
-    digitalWrite(RELAY, HIGH);
+  //  light = analogRead(LIGHT_SENSOR);
+  if (debug) {
+    Serial.print("Tank: ");
+    Serial.print(tank);
+    Serial.print("  Panel: ");
+    Serial.println(panel);
+    // Allow manual control, only applies to this time slice.
+    if (Serial.available() > 0) {
+      int inchar = Serial.read();
+      switch (inchar) {
+      case '1':
+      case 'y':
+      case 'Y':
+        digitalWrite(RELAY, LOW);
+        manual_override = true;
+        Serial.println("Turning relay ON");
+        break;
+      case '0':
+      case 'n':
+      case 'N':
+        digitalWrite(RELAY, HIGH);
+        manual_override = true;
+        Serial.println("Turning relay OFF");
+        break;
+      default:
+         Serial.print(inchar);
+         Serial.println(" Unknown command");  
+      }
+    }
+  }
+  if (! manual_override) {
+    if (/*light > lowLight && */ panel > minTemp && (panel - tank) > minDiff) {
+      digitalWrite(RELAY, LOW);
+    } 
+    else {
+      digitalWrite(RELAY, HIGH);
+    }
   }
 }
 
 void setup() {
   Serial.begin(9600);
-//  ser.begin(9600);
+  //  ser.begin(9600);
   pinMode(RELAY, OUTPUT);
   digitalWrite(RELAY, HIGH);
-//  manager.start(ser);
+  //  manager.start(ser);
   sensors.begin();
   delay(5000);
+  if (debug) {
+    Serial.println("started");
+  }
 }
 
 void loop() {
-//  manager.check();
+  //  manager.check();
   checkInputs();
-  delay(1000);
+  delay(2000);
 }
 
 /*
 vim:ft=cpp ai sw=2 expandtab:
-*/
+ */
+
