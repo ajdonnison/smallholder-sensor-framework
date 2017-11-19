cbfe9424847e3dbf54e85cd1751cc106b1328363
Adam Donnison <ajdonnison@gmail.com>
2017-11-19 22:30:59 +1100

    Add auto-discovery of sensors, config menu.

--- arduino/sketches/Mister/Mister.ino
+++ arduino/sketches/Mister/Mister.ino
@@ -15,10 +15,10 @@
 #include <DelayRun.h>
 #include <SoftTimer.h>
 #include <BlinkTask.h>
+#include <EEPROM.h>
 
 #define ONE_WIRE_IF 2
 #define PUMP_OFFSET 3
-#define DEVICE_COUNT 2
 #define MAX_DEVICE_COUNT 4
 #define MAX_TEMP 36.0
 #define MIN_WAIT 240
@@ -35,32 +35,69 @@ boolean startup_delay = true;
 // Flags
 unsigned char DEBUG = 0;
 unsigned char TESTMODE = 0;
+unsigned char ASSOC_MODE = 0;
 byte currentMenu = 0;
+int currentSensor = 0;
 
 typedef struct st_devlist {
-  DeviceAddress * sensor;
-  int pump;
-  int status;
   float temp;
   int time;
+  uint8_t status;
+  uint8_t fill[3];
 } DEVICE_LIST;
+
+typedef struct _devs_t {
+  DeviceAddress sensor;
+  uint8_t pump;
+  uint8_t enabled;
+  uint16_t fill;
+} devs_t;
+
+struct _cfg {
+  int devcount;
+  devs_t sensors[MAX_DEVICE_COUNT];
+} cfg;
 
 
 DEVICE_LIST nodes[MAX_DEVICE_COUNT];
 
-DeviceAddress  sensor_list[] = {
-  { 0x28, 0x22, 0x5d, 0x97, 0x04, 0x00, 0x00, 0x46 },
-  // { 0x28, 0x79, 0x47, 0x97, 0x04, 0x00, 0x00, 0x51 },
-  { 0x28, 0xc7, 0xff, 0x97, 0x04, 0x00, 0x00, 0x34 }
-};
+void readConfig(void) {
+  EEPROM.get(0, cfg);
+  // Now we build our devices list
+  for (int i = 0; i < MAX_DEVICE_COUNT && i < cfg.devcount; i++) {
+    nodes[i].status = 0;
+    nodes[i].time = 0;
+    nodes[i].temp = 0;
+  }
+}
+
+void writeConfig(void) {
+  EEPROM.put(0, cfg);
+}
+
+void findSensors(void) {
+  int sensor_count;
+
+  // Clear sensor list
+  for (int i = 0; i < MAX_DEVICE_COUNT; i++) {
+    cfg.sensors[i].enabled = 0;
+  }
+
+  sensor_count = devManager.getDeviceCount();
+  for (int i = 0; i < MAX_DEVICE_COUNT && i < sensor_count; i++) {
+    devManager.getAddress(cfg.sensors[i].sensor, i);
+    cfg.sensors[i].enabled = 1;
+  }
+  cfg.devcount = sensor_count;
+}
 
 boolean stopPump(Task *me) {
-  for (int i = 0; i < DEVICE_COUNT; i++) {
+  for (int i = 0; i < cfg.devcount && i < MAX_DEVICE_COUNT; i++) {
     if (nodes[i].status == 1) {
       dprint(F("Pump "));
-      dprint(nodes[i].pump);
+      dprint(cfg.sensors[i].pump + 1 - PUMP_OFFSET);
       dprintln(" Off");
-      digitalWrite(nodes[i].pump, LOW);
+      digitalWrite(cfg.sensors[i].pump, LOW);
       nodes[i].status = 2;
       nodes[i].time = MIN_WAIT;
     }
@@ -77,10 +114,10 @@ void checkTemp(Task *me) {
     return;
   }
   
-  for (int i = 0; i < DEVICE_COUNT; i++) {
-    nodes[i].temp = devManager.getTempC(*(nodes[i].sensor));
+  for (int i = 0; i < cfg.devcount && i < MAX_DEVICE_COUNT; i++) {
+    nodes[i].temp = devManager.getTempC(cfg.sensors[i].sensor);
     dprint(F("Temp "));
-    dprint(nodes[i].pump);
+    dprint(cfg.sensors[i].pump + 1 - PUMP_OFFSET);
     dprint(": ");
     dprintln(nodes[i].temp);
     switch (nodes[i].status) {
@@ -92,9 +129,9 @@ void checkTemp(Task *me) {
       case 0:
         if (nodes[i].temp > MAX_TEMP) {
           dprint(F("Turning on pump "));
-          dprintln(nodes[i].pump);
+          dprintln(cfg.sensors[i].pump + 1 - PUMP_OFFSET);
           nodes[i].status = 1;
-          digitalWrite(nodes[i].pump, HIGH);
+          digitalWrite(cfg.sensors[i].pump, HIGH);
           stopPumpTask.startDelayed();
         }
         break;
@@ -115,20 +152,39 @@ boolean clearDelay(Task *me) {
   return true;
 }
 
+void showSensor(void) {
+  devManager.requestTemperatures();
+  devs_t device = cfg.sensors[currentSensor];
+  float temp = devManager.getTempC(device.sensor);
+  Serial.print(currentSensor);
+  Serial.print(F(" ADDR:"));
+  for (int i = 0; i < sizeof(DeviceAddress); i++) {
+    Serial.print(device.sensor[i], HEX);
+  }
+  Serial.print(F(" PUMP:"));
+  Serial.print(device.pump + 1 - PUMP_OFFSET);
+  Serial.print(F(" TEMP:"));
+  Serial.println(temp);
+}
+
 void showStatus() {
-  if (TESTMODE) {
+  if (TESTMODE || ASSOC_MODE) {
     devManager.requestTemperatures();
   }
   for (int i = 0; i < MAX_DEVICE_COUNT; i++) {
-    if (TESTMODE && i < DEVICE_COUNT) {
-      nodes[i].temp = devManager.getTempC(*(nodes[i].sensor));
+    if ((TESTMODE || ASSOC_MODE) && i < cfg.devcount) {
+      nodes[i].temp = devManager.getTempC(cfg.sensors[i].sensor);
     }
     Serial.print(F("Pump "));
-    Serial.print(i);
+    Serial.print(cfg.sensors[i].pump + 1 - PUMP_OFFSET);
     Serial.print(F(" Pin:"));
-    Serial.print(nodes[i].pump);
+    Serial.print(cfg.sensors[i].pump);
     Serial.print(F(" Temp:"));
     Serial.print(nodes[i].temp);
+    Serial.print(F(" Sensor Addr:"));
+    for (int x = 0; x < sizeof(DeviceAddress); x++) {
+      Serial.print(cfg.sensors[i].sensor[x], HEX);
+    }
     Serial.print(F(" Status:"));
     switch (nodes[i].status) {
       case 0:
@@ -168,12 +224,60 @@ void handleCommand(int cmd) {
 	case 's':
 	  showStatus();
 	  break;
+        case 'a':
+          currentMenu = cmd;
+          ASSOC_MODE = 1;
+          Serial.println(F("Association mode"));
+          break;
 	default:
 	  Serial.println(F("Menu options:"));
 	  Serial.println(F("d - Toggle DEBUG"));
 	  Serial.println(F("t - Test mode"));
 	  Serial.println(F("s - Show Status"));
 	  break;
+      }
+      break;
+    case 'a':
+      switch (cmd) {
+        case 'x':
+          ASSOC_MODE = 0;
+          currentMenu = 0;
+	  writeConfig();
+	  readConfig();
+          Serial.println(F("Run mode"));
+          break;
+        case 's':
+	  findSensors();
+	  currentSensor = 0;
+	  Serial.print(F("Found "));
+	  Serial.print(cfg.devcount);
+	  Serial.println(F(" sensors"));
+	  showSensor();
+          break;
+        case 'n':
+	  if (++currentSensor > cfg.devcount) currentSensor=cfg.devcount;
+	  showSensor();
+          break;
+        case 'p':
+	  if (--currentSensor < 0) currentSensor = 0;
+	  showSensor();
+          break;
+        default:
+          if (cmd >= '1' && cmd < ('1' + MAX_DEVICE_COUNT)) {
+	    int pin = cmd - '1' + PUMP_OFFSET;
+	    if (pin != cfg.sensors[currentSensor].pump) {
+	      cfg.sensors[currentSensor].pump = pin;
+	    }
+	    showSensor();
+          }
+          else {
+            Serial.println(F("Associate options"));
+            Serial.println(F("s - Scan devices"));
+            Serial.println(F("n - Next device"));
+            Serial.println(F("p - Previous device"));
+            Serial.println(F("x - Exit"));
+          }
+          break;
       }
       break;
     case 't':
@@ -187,23 +291,33 @@ void handleCommand(int cmd) {
 	  Serial.println(F("Run mode"));
 	  break;
 	default:
-	  if (cmd >= '0' && cmd < ('0' + DEVICE_COUNT)) {
-	    int i = cmd - '0';
-	    if (nodes[i].status == 1) {
+	  if (cmd >= '1' && cmd < ('1' + MAX_DEVICE_COUNT)) {
+	    int i = cmd - '1';
+            int j = -1;
+            for (int x = 0; x < MAX_DEVICE_COUNT; x++) {
+              if (i == (cfg.sensors[x].pump + 1 - PUMP_OFFSET))
+                j = i;
+                break;
+            }
+            if (j == -1) {
+              Serial.println(F("Cannot find pump"));
+              break;
+            }
+	    if (nodes[j].status == 1) {
 	      Serial.println(F("Turning off pump "));
-	      Serial.print(i);
-	      nodes[i].status = 0;
-	      digitalWrite(nodes[i].pump, LOW);
+	      Serial.print(cfg.sensors[j].pump + 1 - PUMP_OFFSET);
+	      nodes[j].status = 0;
+	      digitalWrite(cfg.sensors[j].pump, LOW);
 	    } else {
 	      Serial.println(F("Turning on pump "));
-	      Serial.print(i);
+	      Serial.print(cfg.sensors[j].pump + 1 - PUMP_OFFSET);
 	      nodes[i].status = 1;
-	      digitalWrite(nodes[i].pump, HIGH);
+	      digitalWrite(cfg.sensors[j].pump, HIGH);
 	    }
 	  }
 	  else {
 	    Serial.println(F("Test options:"));
-	    Serial.println(F("0-9 - toggle pump status"));
+	    Serial.println(F("1-9 - toggle pump status"));
 	    Serial.println(F("s - Show Status"));
 	    Serial.println(F("x - Exit test mode"));
 	  }
@@ -226,7 +340,6 @@ Task menuHandler(100, kint);
 DelayRun clearDelayTask(6000, clearDelay);
 
 void setup() {
-  // Set up the nodes array
   Serial.begin(9600);
   Serial.println("Starting");
   
@@ -237,18 +350,16 @@ void setup() {
     pinMode(pin, OUTPUT);
     digitalWrite(pin, LOW);
   }
-  for (int i = 0; i < MAX_DEVICE_COUNT; i++) {
-    if (i < DEVICE_COUNT) {
-      nodes[i].sensor = sensor_list+i;
-    }
-    nodes[i].pump = PUMP_OFFSET + i;
-    nodes[i].temp = 0.0;
-    nodes[i].status = 0;
-    nodes[i].time = 0;
-  }
+  // Check config, set up nodes array
+  readConfig();
   devManager.begin();
   SoftTimer.add(&checkTempTask);
   SoftTimer.add(&menuHandler);
+  // Now see if we need to drop into config mode
+  if (cfg.devcount <= 0 || cfg.devcount > MAX_DEVICE_COUNT) {
+    currentMenu = 'a';
+    ASSOC_MODE = 1;
+  }
   clearDelayTask.startDelayed();
 }
 
